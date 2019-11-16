29ea9111473334c1fc05de9574906f91b170a07b
Benoit Eudier <benoit.eudier@gmail.com>
2019-11-16 10:46:19 +0900

    Now shoot in the correct direction

--- src/systems/attack.rs
+++ src/systems/attack.rs
@@ -2,13 +2,20 @@
 //! Player will be looking in the direction of the mouse. It will attack on mouse click
 
 use amethyst::{
-    core::{math::Vector3, timing::Time, SystemDesc, Transform},
+    core::{
+        geometry::Plane,
+        math::{Point2, Vector2},
+        timing::Time,
+        SystemDesc, Transform,
+    },
     derive::SystemDesc,
     ecs::{
-        Component, Entities, Join, LazyUpdate, Read, ReadStorage, System, SystemData, VecStorage,
-        World, Write, WriteStorage,
+        Component, Entities, Join, LazyUpdate, Read, ReadExpect, ReadStorage, System, SystemData,
+        VecStorage, World, Write, WriteStorage,
     },
     input::{InputHandler, StringBindings},
+    renderer::camera::{ActiveCamera, Camera},
+    window::ScreenDimensions,
     winit::MouseButton,
 };
 
@@ -43,6 +50,10 @@ impl<'s> System<'s> for AttackSystem {
         WriteStorage<'s, Weapon>,
         Read<'s, InputHandler<StringBindings>>,
         Read<'s, Time>,
+        // This is necessary for getting the mouse position projected on the camera view.
+        ReadStorage<'s, Camera>,
+        ReadExpect<'s, ScreenDimensions>,
+        Read<'s, ActiveCamera>,
         // This is necessary to spawn bullets.
         Entities<'s>,
         Read<'s, LazyUpdate>,
@@ -58,6 +69,9 @@ impl<'s> System<'s> for AttackSystem {
             mut weapon,
             input,
             time,
+            cameras,
+            screen_dimensions,
+            active_camera,
             entities,
             updater,
             bullet_spawner,
@@ -73,29 +87,42 @@ impl<'s> System<'s> for AttackSystem {
                 0.0f32.max(weapon.time_before_shooting - time.delta_seconds());
 
             if input.mouse_button_is_down(MouseButton::Left) {
-                debug!(
-                    "Mouse click at {:?} (player is at {:?}",
-                    input.mouse_position(),
-                    transform.translation()
-                );
-
                 if let Some((x, y)) = input.mouse_position() {
-                    if weapon.time_before_shooting <= 0.0 {
-                        let d = (Vector3::new(x, y, 0.0) - transform.translation())
-                            .xy()
-                            .normalize();
-                        if let Err(e) = bullet_spawner.spawn_player_bullet(
-                            &entities,
-                            &updater,
-                            &mut collision_world,
-                            0,
-                            *transform.translation(),
-                            d,
-                            100.0,
-                        ) {
-                            error!("Error while spawning player bullet = {:?}", e);
+                    let mut camera_join = (&cameras, &transforms).join();
+                    if let Some((camera, camera_transform)) = active_camera
+                        .entity
+                        .and_then(|a| camera_join.get(a, &entities))
+                        .or_else(|| camera_join.next())
+                    {
+                        // Project a ray from the camera to the 0z axis
+                        let ray = camera.projection().screen_ray(
+                            Point2::new(x, y),
+                            Vector2::new(screen_dimensions.width(), screen_dimensions.height()),
+                            camera_transform,
+                        );
+                        let distance = ray.intersect_plane(&Plane::with_z(0.0)).unwrap();
+                        let mouse_world_position = ray.at_distance(distance);
+                        debug!(
+                            "Mouse click at {:?} (player is at {:?}",
+                            mouse_world_position,
+                            transform.translation()
+                        );
+                        if weapon.time_before_shooting <= 0.0 {
+                            let d = mouse_world_position - transform.translation();
+                            let d = Vector2::new(d.x, d.y).normalize();
+                            if let Err(e) = bullet_spawner.spawn_player_bullet(
+                                &entities,
+                                &updater,
+                                &mut collision_world,
+                                0,
+                                *transform.translation(),
+                                d,
+                                100.0,
+                            ) {
+                                error!("Error while spawning player bullet = {:?}", e);
+                            }
+                            weapon.time_before_shooting = weapon.reload_time;
                         }
-                        weapon.time_before_shooting = weapon.reload_time;
                     }
                 }
             }
